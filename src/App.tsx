6ef93fdbe44c37584b7ccc5327d1817055715e7e
rawphl <ivancammarota@gmail.com>
2021-01-12 10:46:40 +0100

    PW

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { useFavorites, useBadis } from "./store/index"
 import './App.css';
+import { Badi } from './models/Badi';
+
+type BadiListProps = {
+  badis: any[]
+}
+
+const BadiList: React.FC<BadiListProps> = ({ badis }) => {
+  const { addFavorite, removeFavorite, isFavorite } = useFavorites()
+  const toggleFavorite = (id: number) => {
+    isFavorite(id) ? removeFavorite(id) : addFavorite(id)
+  }
+
+  const [favoritesOnly, setFavoritesOnly] = useState(false)
+  const [favoritedBadis, setFavoritedBadis] = useState<Badi[]>([])
+  const toggleFavoritesOnly = () => {
+    setFavoritesOnly(!favoritesOnly)
+  }
+
+  useEffect(() => {
+    setFavoritedBadis(favoritesOnly ? badis.filter(badi => isFavorite(badi.badid)) : [])
+  }, [favoritesOnly])
+
+  const badisToRender = favoritesOnly ? favoritedBadis : badis
+  return (
+    <>
+      <div>
+        <button onClick={(e) => toggleFavoritesOnly()}>Toggle favorites only</button>
+      </div>
+      <ul>
+        {
+          badisToRender.map((badi) => {
+            return (
+              <li key={badi.badid} onClick={e => toggleFavorite(badi.badid)}>
+                <h4>{badi.badname}
+                  <span>
+                    {isFavorite(badi.badid) ? " ★" : " ☆"}
+                  </span>
+                </h4>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </>
+  )
+}
 
 function App() {
+  const { isLoading, error, badis } = useBadis()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Badis</h1>
+      <h3 style={{color: "red"}}>{navigator.onLine ? "" : " offline modus"}</h3>
+      { isLoading && <h1>Loading badis...</h1>}
+      <BadiList badis={badis} />
     </div>
   );
 }
---
+++ src/store/index.ts
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react"
+import { loadBadis } from "../api"
+import type { Badi } from "../models/Badi"
+
+const FAVORITES_KEY = "favorites"
+
+type FavoritesHookReturnValue = {
+    addFavorite: (id: number) => void,
+    removeFavorite: (id: number) => void,
+    isFavorite: (id: number) => boolean
+}
+
+export const useFavorites: () => FavoritesHookReturnValue = () => {
+    const [favorites, setFavorites] = useState<number[]>([])
+
+    useEffect(() => {
+        const maybeFavorites = localStorage.getItem(FAVORITES_KEY)
+        setFavorites(maybeFavorites !== null ? JSON.parse(maybeFavorites) : [])
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem(FAVORITES_KEY, JSON.stringify(favorites))
+    }, [favorites])
+
+    const addFavorite = (id: number) => setFavorites([...favorites, id])
+    const removeFavorite = (id: number) => setFavorites(favorites.filter(fid => fid !== id))
+    const isFavorite = (id: number) => favorites.includes(id)
+
+    return { addFavorite, removeFavorite, isFavorite }
+}
+
+const BADIS_KEY = "badis"
+
+type BadiHookReturnValue = {
+    isLoading: boolean,
+    error: string,
+    badis: Badi[]
+}
+
+const saveBadis = (badis: Badi[]) => localStorage.setItem(BADIS_KEY, JSON.stringify(badis))
+const loadSavedBadis = () => {
+    return localStorage.getItem(BADIS_KEY) ? JSON.parse(localStorage.getItem(BADIS_KEY)!) : []
+}
+
+export const useBadis: () => BadiHookReturnValue = () => {
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState("")
+    const [badis, setBadis] = useState<Badi[]>(loadSavedBadis())
+    useEffect(() => saveBadis(badis), [badis])
+
+    useEffect(() => {
+        if (isLoading || !navigator.onLine) return
+        const load = async () => {
+            setIsLoading(true)
+            try {
+                const badis = await loadBadis()
+                setBadis(badis)
+            } catch (e) {
+                setError("Could not load badis")
+            } finally {
+                setIsLoading(false)
+            }
+        }
+        load()
+    }, [])
+
+
+    return { isLoading, error, badis }
+}
